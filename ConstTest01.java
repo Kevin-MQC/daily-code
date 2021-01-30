6a148116e86accd0cb51c5ddf4bf7488db3b4b85
MQCKevin <1667439910@qq.com>
2021-01-30 09:16:24 +0800

    为了尝试第一次使用上传

---
+++ ConstTest01.java
@@ -0,0 +1,11 @@
+public class ConstTest01
+{
+public static void main(String[] args)
+{
+System.out.println("abc");
+System.out.println("Œ“");
+
+System.out.println("A");
+System.out.println(false);
+}
+}
