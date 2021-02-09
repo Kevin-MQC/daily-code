e914fce929eec56d27ed7f92c0c4c56a89e75a8b
MQCKevin <1667439910@qq.com>
2021-02-09 16:07:03 +0800

    第一次用java的输入语句，跟c语言的输入语句相差的很大啊，慢慢的适应吧。

---
+++ KeyinputTest.java
@@ -0,0 +1,10 @@
+public class KeyinputTest
+
+{
+	public static void main(String[] args){
+	java.util.Scanner s = new java.util.Scanner(System.in);
+	
+	String userInputContent = s.next();
+	System.out.println("ÄúÊäÈëÁË£º"+userInputContent);
+	}
+}
