6659dfbc3da061c34c7e537ab14be1c61d54e0f3
MQCKevin <1667439910@qq.com>
2021-02-09 15:40:48 +0800

    If1

---
+++ IfTest.java
@@ -0,0 +1,25 @@
+
+public class IfTest
+{
+	public static void main(String[] args)
+	{
+		
+          int grade =90;
+		  
+		  if(grade >=90){
+			  System.out.println('A');
+		  }else if (grade >=80){
+			  System.out.println('B');
+		  }else if (grade >=70)
+		  {
+			  System.out.println('C');
+		  }
+		  else System.out.println('D');
+		 
+		  
+
+
+
+
+	}
+}
