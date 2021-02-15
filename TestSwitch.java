087156677511c8990569e45c2ea9761eb55b0e87
MQCKevin <1667439910@qq.com>
2021-02-15 11:40:22 +0800

    使用Switch

---
+++ TestSwitch.java
@@ -0,0 +1,18 @@
+public class TestSwitch
+{
+	public static void main(String[] args){
+		
+		byte age=40;
+		switch(age){
+			case 30 :
+				System.out.println("你是青年人哦");
+			case 50 :
+				System.out.println("你是中年人哦");
+			default :
+				System.out.println("你不在我们考察的范围");
+		}
+		
+		
+	}
+
+}
