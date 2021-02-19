8b5f61f6fb18074277b66e67b8aa21b9406daadd
MQCKevin <1667439910@qq.com>
2021-02-19 11:43:35 +0800

    方法，也就是c语言中的函数

---
+++ method.java
@@ -0,0 +1,15 @@
+public class method
+{
+	public static void main(String[] args){
+		
+		method.sumint(10,20);
+		method.sumint(3,23);
+		
+
+	}
+	public static void sumint(int a,int b){
+		int c=a+b;
+		System.out.println(a+"+"+b+"="+c);
+	}
+
+}
