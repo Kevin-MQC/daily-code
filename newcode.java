5f21e45ac4122dd5f20315b7f57cad8ca3554bc2
MQCKevin <1667439910@qq.com>
2021-03-04 20:38:36 +0800

    好久没有上传代码，水一次

---
+++ newcode.java
@@ -0,0 +1,9 @@
+public class newcode
+{
+	public static void main(String[] args){
+		String a ="好久都没有xie code ";
+		String b ="shang chaunyige";
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
